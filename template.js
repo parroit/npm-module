068a4b03f6f8c5e23eff2f4325a65693aa22c04d
parroit <andrea.parodi@ebansoftware.net>
2013-11-30 20:16:09 +0100

    added test

--- template.js
+++ template.js
@@ -13,61 +13,66 @@ exports.description = 'Create a Node.js module, including Mocha unit tests.';
 
 // Template-specific notes to be displayed before question prompts.
 exports.notes = '_Project name_ shouldn\'t contain "node" or "js" and should ' +
-  'be a unique ID not already in use at search.npmjs.org.';
+    'be a unique ID not already in use at search.npmjs.org.';
 
 // Template-specific notes to be displayed after question prompts.
 exports.after = 'You should now install project dependencies with _npm ' +
-  'install_. After that, you may execute project tasks with _grunt_. For ' +
-  'more information about installing and configuring Grunt, please see ' +
-  'the Getting Started guide:' +
-  '\n\n' +
-  'http://gruntjs.com/getting-started';
+    'install_. After that, you may execute project tasks with _grunt_. For ' +
+    'more information about installing and configuring Grunt, please see ' +
+    'the Getting Started guide:' +
+    '\n\n' +
+    'http://gruntjs.com/getting-started';
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
 
 // The actual init template.
-exports.template = function(grunt, init, done) {
+exports.template = function (grunt, init, done) {
 
-  init.process({type: 'node'}, [
-    // Prompt for these values.
-    init.prompt('name'),
-    init.prompt('description'),
-    init.prompt('version'),
-    init.prompt('repository'),
-    init.prompt('homepage'),
-    init.prompt('bugs'),
-    init.prompt('licenses'),
-    init.prompt('author_name'),
-    init.prompt('author_email'),
-    init.prompt('author_url'),
-    init.prompt('node_version', '>= 0.10.0'),
-    init.prompt('main')
+    init.process({type: 'node'}, [
+        // Prompt for these values.
+        init.prompt('name'),
+        init.prompt('description'),
+        init.prompt('version'),
+        init.prompt('repository'),
+        init.prompt('homepage'),
+        init.prompt('bugs'),
+        init.prompt('licenses'),
+        init.prompt('author_name'),
+        init.prompt('author_email'),
+        init.prompt('author_url'),
+        init.prompt('node_version', '>= 0.10.0'),
+        init.prompt('main')
 
-  ], function(err, props) {
-    props.keywords = [];
-    props.devDependencies = {
-        "grunt": "~0.4.1",
-        "grunt-mocha-test": "~0.7.0",
-        "expect.js": "~0.2.0",
-        "grunt-contrib-watch": "~0.5.3"
-    };
-    props.travis = true;
+    ], function (err, props) {
+        props.keywords = [];
+        props.devDependencies = {
+            "grunt": "~0.4.1",
+            "grunt-mocha-test": "~0.7.0",
+            "expect.js": "~0.2.0",
+            "grunt-contrib-watch": "~0.5.3"
+        };
 
-    // Files to copy (and process).
-    var files = init.filesToCopy(props);
+        props.scripts = {
+            "test": "grunt test"
+        };
 
-    // Add properly-named license files.
-    init.addLicenseFiles(files, props.licenses);
+        props.travis = true;
 
-    // Actually copy (and process) files.
-    init.copyAndProcess(files, props);
+        // Files to copy (and process).
+        var files = init.filesToCopy(props);
 
-    // Generate package.json file.
-    init.writePackageJSON('package.json', props);
+        // Add properly-named license files.
+        init.addLicenseFiles(files, props.licenses);
 
-    // All done!
-    done();
-  });
+        // Actually copy (and process) files.
+        init.copyAndProcess(files, props);
+
+        // Generate package.json file.
+        init.writePackageJSON('package.json', props);
+
+        // All done!
+        done();
+    });
 
 };
